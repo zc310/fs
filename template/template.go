7be2b1452a947323bb5ac8c1959537d95a6498bd
zc310 <liushiyan@msn.com>
2017-08-16 14:51:48 +0800

    新增 Template

---
+++ template/template.go
@@ -0,0 +1,81 @@
+package template
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+
+	"github.com/valyala/bytebufferpool"
+	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasttemplate"
+	"github.com/zc310/utils/fasthttputil"
+	"sync"
+)
+
+type Template struct {
+	ctx *fasthttp.RequestCtx
+	buf bytebufferpool.Pool
+}
+
+var pool = &sync.Pool{
+	New: func() interface{} {
+		return New(nil)
+	},
+}
+
+func Get() *Template {
+	return pool.Get().(*Template)
+}
+func Put(t *Template) {
+	pool.Put(t)
+}
+func New(ctx *fasthttp.RequestCtx) *Template {
+	return &Template{ctx: ctx}
+}
+func (p *Template) SetCtx(ctx *fasthttp.RequestCtx) {
+	p.ctx = ctx
+}
+func (p *Template) Execute(t string) (string, error) {
+	buf := bytebufferpool.Get()
+	defer bytebufferpool.Put(buf)
+	_, err := fasttemplate.New(t, "{", "}").ExecuteFunc(buf, (func(w io.Writer, tag string) (int, error) {
+		switch tag {
+		case "tempdir":
+			return w.Write([]byte(os.TempDir()))
+		default:
+			if p.ctx != nil {
+				switch tag {
+				case "scheme":
+					return w.Write(p.ctx.URI().Scheme())
+				case "host":
+					return w.Write(p.ctx.Host())
+				case "request_uri":
+					return w.Write(p.ctx.URI().FullURI())
+				case "request_method":
+					return w.Write(p.ctx.Method())
+				case "request_body":
+					return w.Write(p.ctx.Request.Body())
+				case "content_type":
+					return w.Write(p.ctx.Request.Header.Peek("Content-Type"))
+				case "content_length":
+					return w.Write([]byte(strconv.Itoa(len(p.ctx.PostBody()))))
+				case "query_string":
+					return w.Write(p.ctx.Request.URI().QueryString())
+				case "document_root":
+					return w.Write(p.ctx.Path())
+				case "remote_addr":
+					return w.Write([]byte(p.ctx.RemoteIP().String()))
+				default:
+					return w.Write(fasthttputil.GetArgs(p.ctx, tag))
+				}
+			}
+			return 0, fmt.Errorf("[unknown tag %q]", tag)
+		}
+	}))
+	return buf.String(), err
+}
+
+func NewTemplate(t string) (string, error) {
+	return New(nil).Execute(t)
+}
---
+++ template/template_test.go
@@ -0,0 +1,46 @@
+package template
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/valyala/fasthttp"
+)
+
+func testExecute(t *testing.T, template *Template, s, out string) {
+	output, err := template.Execute(s)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, output, out)
+}
+func TestTemplate_Execute(t *testing.T) {
+	var ctx fasthttp.RequestCtx
+	var req fasthttp.Request
+	req.Header.SetHost("a.com")
+	req.Header.SetMethod("POST")
+	req.Header.Set("User-Agent", "IE10")
+	req.Header.Set("Content-Type", "image/svg+xml")
+	req.Header.Set("Referer", "http://b.com")
+	req.SetRequestURI("/a/b/c?asdfasdfas=asdfasdsa+d+f%09%27%27dd&a1=123")
+	ctx.Init(&req, nil, nil)
+	fmt.Fprint(ctx.Request.BodyWriter(), "123456789")
+
+	tmp := New(nil)
+	testExecute(t, tmp, "{tempdir}", os.TempDir())
+
+	tmp = New(&ctx)
+	testExecute(t, tmp, "{scheme}", "http")
+	testExecute(t, tmp, "{host}", "a.com")
+	testExecute(t, tmp, "{request_uri}", "http://a.com/a/b/c?asdfasdfas=asdfasdsa+d+f%09%27%27dd&a1=123")
+	testExecute(t, tmp, "{request_method}", "POST")
+	testExecute(t, tmp, "{request_body}", "123456789")
+	testExecute(t, tmp, "{content_type}", "image/svg+xml")
+	testExecute(t, tmp, "{content_length}", "9")
+	testExecute(t, tmp, "{query_string}", "asdfasdfas=asdfasdsa+d+f%09%27%27dd&a1=123")
+	testExecute(t, tmp, "{document_root}", "/a/b/c")
+	testExecute(t, tmp, "{remote_addr}", "0.0.0.0")
+	testExecute(t, tmp, "{a1}", "123")
+	testExecute(t, tmp, "{User-Agent}", "IE10")
+	testExecute(t, tmp, "{Referer}", "http://b.com")
+}
