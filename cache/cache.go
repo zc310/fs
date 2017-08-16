af1554c17cfacc721765aac09071175ed1d4e711
zc310 <liushiyan@msn.com>
2017-08-16 14:53:40 +0800

    fmt

--- cache/cache.go
+++ cache/cache.go
@@ -1,6 +1,8 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
 
 //Cache interface
 type Cache interface {
@@ -9,7 +11,7 @@ type Cache interface {
 	// GetRange
 	GetRange(key string, low, high int64) (value []byte, ok bool)
 	// Put set cached value with key and expire time
-	Set(key string, value []byte, timeout time.Duration)(err error)
+	Set(key string, value []byte, timeout time.Duration) (err error)
 	// Delete delete cached value by key
 	Delete(key string) (err error)
 	// ClearAll clear all cache
