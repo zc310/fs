a268ef5750066e7c5e71aec885ffb642684b0293
zc310 <liushiyan@msn.com>
2017-08-31 19:07:41 +0800

    add path hit info

--- middleware/helloworld.go
+++ middleware/helloworld.go
@@ -1,29 +1,39 @@
 package middleware
 
 import (
+	"encoding/json"
 	"fmt"
-	"sync/atomic"
+	"sync"
 	"time"
 
 	"github.com/valyala/fasthttp"
 )
 
 type Helloworld struct {
-	i int64
+	hit  map[string]int64
+	lock sync.Mutex
 }
 
-func (p *Helloworld) Init(c *Config) (err error) { return nil }
-func (p *Helloworld) UnInit()                    {}
+func (p *Helloworld) Init(c *Config) (err error) {
+	p.hit = map[string]int64{}
+	return nil
+}
+func (p *Helloworld) UnInit() {}
 func (p *Helloworld) Handler() fasthttp.RequestHandler {
 	return func(ctx *fasthttp.RequestCtx) {
-		fmt.Fprintf(ctx, "Hello, world!\n\n%d\n", p.i)
+		fmt.Fprintf(ctx, "Hello, world!\n\n%d\n", p.hit[string(ctx.Request.URI().Path())])
 		ctx.SetContentType("text/plain; charset=utf8")
 		fmt.Fprintf(ctx, "%s\n\n", &ctx.Request)
 		var c fasthttp.Cookie
 		c.SetKey("cookie-name")
 		c.SetValue(time.Now().String())
 		ctx.Response.Header.SetCookie(&c)
-		atomic.AddInt64(&p.i, 1)
+		p.lock.Lock()
+		p.hit[string(ctx.Request.URI().Path())]++
+		b, _ := json.MarshalIndent(p.hit, " ", " ")
+		fmt.Fprintf(ctx, "\n\n%s", string(b))
+		p.lock.Unlock()
+
 	}
 }
 
